1320c44ba256dcad7c1a17666e4e67d9651ee949
Ian A <iastesana@gmail.com>
2020-10-28 11:36:07 -0700

    created routes, model, and server.js and am successfully running the skeleton of my code.

---
+++ routes/api.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const Transaction = require("../models/workoutmodel.js");
+// prototyping model
+
+router.post("/api/workouts", ({ body }, res) => {
+    Transaction.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Transaction.find({})
+        .sort({ date: -1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongojs = require("mongojs");
 const logger = require("morgan");
 const mongoose = require("mongoose")
 
+const PORT = process.env.PORT || 3000;
+
 const databaseUrl = "workout";
 const collections = ["test"];
 const db = mongojs(databaseUrl, collections);
@@ -19,10 +21,13 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
     useFindAndModify: false
 });
 
+app.use(require("./routes/api.js"));
+
+
 db.on("error", error => {
     console.log("Database Error:", error);
 });
 
-app.listen(3000, () => {
-    console.log("App running on port 3000!");
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
 });
